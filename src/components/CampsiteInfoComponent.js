0d5ae84644f6b0aab6ad167c64ffd35de713f809
Alfira Gutierrez <rizalfira0503@gmail.com>
2021-08-07 12:05:17 -0700

    Week 4 Workshop Assignment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,106 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, {Component} from 'react';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, 
+    Button,Modal, ModalHeader, ModalBody, FormGroup, Label} from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+
+
+const required = val => val && val.length;
+const maxLength = len => val => !val || val.length <= len;
+const minLength = len => val => val && val.length >= len;
+
+class CommentForm extends Component {
+    constructor(props) {
+      super(props);
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.toggleModal = this.toggleModal.bind(this);
+      this.state = {
+        isModalOpen: false
+      };
+    }
+  
+    toggleModal() {
+      this.setState({
+        isModalOpen: !this.state.isModalOpen
+      });
+    }
+  
+    handleSubmit(values) {
+        console.log("Current State is:" + JSON.stringify(values));
+        alert("Current State is:" + JSON.stringify(values));
+    }
+  
+    render() {
+      return (
+        <div>
+          <Button outline onClick={this.toggleModal}>
+            <span className="fa fa-pencil fa-lg" /> Submit Comment
+          </Button>
+          <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+            <ModalHeader toggle={this.toggleModal}>Submit comment</ModalHeader>
+            <ModalBody>
+              <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                <FormGroup>
+                  <Label htmlFor="rating">Rating</Label>
+                  <Control.select
+                    type="select"
+                    model=".rating"
+                    name="rating"
+                    id="rating"
+                    className="form-control"
+                  >
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                  </Control.select>
+                </FormGroup>
+                <FormGroup>
+                  <Label htmlFor="author">Your Name</Label>
+                  <Control.text
+                    name="author"
+                    id="author"
+                    model=".author"
+                    placeholder="Your Name"
+                    className="form-control"
+                    validators={{
+                      required,
+                      minLength: minLength(2),
+                      maxLength: maxLength(15)
+                    }}
+                  />
+                  <Errors
+                    className="text-danger"
+                    model=".author"
+                    show="touched"
+                    messages={{
+                      required: "Required",
+                      minLength: "Must be greater than 2 characters",
+                      maxLength: "Must be 15 characters or less"
+                    }}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label htmlFor="text">Comment</Label>
+                  <Control.textarea
+                    rows="6"
+                    name="text"
+                    id="text"
+                    model=".text"
+                    className="form-control"
+                  />
+                </FormGroup>
+                <Button color="primary" type="submit">
+                  Submit
+                </Button>
+              </LocalForm>
+            </ModalBody>
+          </Modal>
+        </div>
+      );
+    }
+  }
 
 function RenderCampsite({campsite}) {
     return (
@@ -34,6 +134,7 @@ function RenderComments({comments}){
                         )
                     })
                 }
+                 <CommentForm/>   
             </div>
         )
     }
