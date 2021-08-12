ca33680d9bd969519a6f72fff4c65963320638b6
Alfira Gutierrez <rizalfira0503@gmail.com>
2021-08-12 16:46:30 -0700

    Redux Thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem,
     Button,Modal, ModalHeader, ModalBody, FormGroup, Label} from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 
 const required = val => val && val.length;
@@ -141,7 +142,27 @@ function RenderComments({comments, addComment, campsiteId}) {
 }
 
 function CampsiteInfo(props) {
-    if (props.campsite) {
+  if (props.isLoading) {
+      return (
+          <div className="container">
+              <div className="row">
+                  <Loading />
+              </div>
+          </div>
+      );
+  }
+  if (props.errMess) {
+      return (
+          <div className="container">
+              <div className="row">
+                  <div className="col">
+                      <h4>{props.errMess}</h4>
+                  </div>
+              </div>
+          </div>
+      );
+  }
+  if (props.campsite) {
         return (
             <div className="container">
                 <div className="row">
