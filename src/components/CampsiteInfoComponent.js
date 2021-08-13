a831c7e40d7c1f10b828584a6de87d7b16841033
Alfira Gutierrez <rizalfira0503@gmail.com>
2021-08-13 11:15:44 -0700

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -29,7 +29,7 @@ class CommentForm extends Component {
   
     handleSubmit(values) {
       this.toggleModal();
-      this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+      this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
   }
   
     render() {
@@ -118,7 +118,7 @@ class CommentForm extends Component {
 }
 
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     if(comments){
         return (
             <div className="col-md-5 m-1">
@@ -136,7 +136,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                         )
                     })
                 }
-                 <CommentForm campsiteId={campsiteId} addComment={addComment} />  
+                 <CommentForm campsiteId={campsiteId} postComment={postComment} /> 
             </div>
         )
     }
@@ -178,9 +178,9 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments 
+                    <RenderComments
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
                     />
                 </div>
